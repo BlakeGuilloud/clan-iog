a1ab2f4e212acf21967436833662c6ea7b29e116
Blake Guilloud <blake.guilloud@gmail.com>
2018-03-12 20:10:49 -0400

    Blake - getting started on a new feature.

---
+++ api/article-service/dist/handler.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _mod = require('./mod');
+
+module.exports = {
+  postArticle: function postArticle() {
+    return console.log('Doggy dog', _mod.dog);
+  }
+};
---
+++ api/article-service/lib/handler.js
@@ -0,0 +1,5 @@
+import { dog } from './mod';
+
+module.exports = {
+  postArticle: () => console.log('Doggy dog', dog),
+};
